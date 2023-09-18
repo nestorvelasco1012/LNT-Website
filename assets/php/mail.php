cd378decfa9251ada0f5ebac48ae610e52a6e7db
nestorvelasco12 <nava822@hotmail.com>
2023-09-18 12:12:35 -0500

    Footer and header fix on all tabs

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -19,7 +19,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "info@yoursite.com";
+        $recipient = "lanuevatortilleriabx@yahoo.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
